d987195a3b20d04edd1ab653bbda3affab7c4f1d
Pranita0306 <159889169+Pranita0306@users.noreply.github.com>
2024-08-25 23:02:00 -0700

    Add files via upload

---
+++ EXP_4.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+import pandas as pd
+Boston=pd.read_csv('./BostonHousing.csv')
+Boston.head()
+
+
+# In[17]:
+
+
+import pandas as pd
+
+
+# In[34]:
+
+
+# Applying LR algorithm
+y=Boston[['medv']] # dependant variable
+x=Boston[['crim']]  # independant variable
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
+lr=LinearRegression()
+lr.fit(x_train,y_train)
+y_pred=lr.predict(x_test)
+y_test.head()
+
+
+# In[35]:
+
+
+y_pred[0:8]
+
+
+# In[36]:
+
+
+from sklearn.metrics import mean_squared_error
+mean_squared_error(y_test,y_pred)
+
+
+# In[37]:
+
+
+x=Boston[['lstat']]
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
+lr2=LinearRegression()
+lr2.fit(x_train,y_train)
+y_pred2=lr2.predict(x_test)
+from sklearn.metrics import mean_squared_error
+mean_squared_error(y_test,y_pred2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
